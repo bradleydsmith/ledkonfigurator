024992a918e61d161e158ef7ea587bedda88c804
Bradley Smith <s3318889@student.rmit.edu.au>
2018-09-14 19:48:06 +1000

    Add menu menu and quit functionality

--- led_konfigurator.sh
+++ led_konfigurator.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+LEDPATH=/sys/class/leds
+
+main() {
+	
+	while true; do
+		echo 'Welcome to Led_Konfigurator!'
+		echo '============================'
+		local ledFiles=()
+		local ledFilesCount=0
+		for file in $LEDPATH/*; do
+			local led=${file##*/}
+			ledFiles+=led
+			((ledFilesCount++))
+			echo "${ledFilesCount}. ${led}"
+		done
+		local quitNo=$((ledFilesCount+1))
+		echo "${quitNo}. Quit"
+		read -p "Please enter a number (1-${quitNo}) for the led to configure or quit: " selection
+	
+		if [[  $selection = $quitNo ]]; then
+			echo "Quit"
+			exit 0
+		fi
+	done
+}
+
+main
