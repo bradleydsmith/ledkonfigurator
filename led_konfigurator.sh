6e8f0fb0c22e914d4954edb520d877cfecf43b95
Bradley Smith <s3318889@student.rmit.edu.au>
2018-09-14 20:31:09 +1000

    Add LED Manipulation Menu

--- led_konfigurator.sh
+++ led_konfigurator.sh
@@ -11,18 +11,48 @@ main() {
 		local ledFilesCount=0
 		for file in $LEDPATH/*; do
 			local led=${file##*/}
-			ledFiles+=led
+			ledFiles[ledFilesCount]=${led}
 			((ledFilesCount++))
 			echo "${ledFilesCount}. ${led}"
 		done
 		local quitNo=$((ledFilesCount+1))
 		echo "${quitNo}. Quit"
+		
+		local selection
 		read -p "Please enter a number (1-${quitNo}) for the led to configure or quit: " selection
-	
 		if [[  $selection = $quitNo ]]; then
 			echo "Quit"
 			exit 0
 		fi
+		
+		ledManipulationMenu ${ledFiles[$((selection-1))]}
+	done
+}
+
+ledManipulationMenu() {
+	local led=$1
+	local selection
+	
+	while true; do
+		echo '['"${led}"']'
+		echo '=========='
+		echo 'What would you like to do with this led?'
+		echo '1. Turn On'
+		echo '2. Turn Off'
+		echo '3. Associate with a system event'
+		echo '4. Associate with the performance of a process'
+		echo '5. Stop association with a process performance'
+		echo '6. Quit to main menu'
+		read -p 'Please enter a number (1-6) for your choice: ' selection
+	
+		case "$selection" in
+			1|2|3|4|5)
+				echo 'Unimplemented'
+				;;
+			6)
+				return 0
+				;;
+		esac
 	done
 }
 
