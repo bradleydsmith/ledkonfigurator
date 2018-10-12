8c28f29280b7e57b4c439a7c690dca31b3f03f77
Bradley Smith <s3318889@student.rmit.edu.au>
2018-10-13 10:17:39 +1100

    Shellcheck Process Monitor

--- process_monitor_led_konfigurator.sh
+++ process_monitor_led_konfigurator.sh
@@ -8,7 +8,8 @@ main() {
 	local processName
 	local monitor
 	local led
-	local usage=$(echo "Usage:" ${0##*/} "[-l led name] [-p process name] [-m montor value]")
+	local usage
+	usage="Usage: ${0##*/} [-l led name] [-p process name] [-m montor value]"
 	while getopts "l:p:m:" opt; do
 		case "$opt" in
 			l)
@@ -21,7 +22,7 @@ main() {
 				monitor=$OPTARG
 				;;
 			?)
-				echo "Usage:" ${0##*/} "[-l led name] [-p process name] [-m montor value]"
+				echo "$usage"
 				return 1
 				;;
 		esac
@@ -29,34 +30,34 @@ main() {
 	
 	if [ -z "$led" ]; then
 		echo 'error: must provide a led name'
-		echo $usage
+		echo "$usage"
 		return 1
 	fi
 	
 	if [ -z "$processName" ]; then
 		echo 'error: must provide a process name'
-		echo $usage
+		echo "$usage"
 		return 1
 	fi
 	
 	if [ -z "$monitor" ]; then
 		echo 'error: must provide a monitor'
-		echo $usage
+		echo "$usage"
 		return 1
 	fi
 	
 	if [[ $monitor != "cpu" && $monitor != "memory" ]]; then
 		echo 'error: monitor value must be cpu or memory'
-		echo $usage
+		echo "$usage"
 		return 1
 	fi
 	
 	if [[ $monitor = "cpu" ]]; then
-		monitorCPU $processName $led
+		monitorCPU "$processName" "$led"
 	fi
 	
 	if [[ $monitor = "memory" ]]; then
-		monitorMemory $processName $led
+		monitorMemory "$processName" "$led"
 	fi
 }
 
@@ -75,29 +76,41 @@ ledTurnOn() {
 }
 
 monitorCPU() {
-	setTrigger $2 "none"
+	setTrigger "$2" "none"
 	while true; do
-		local processCPUUsage=($(ps aux | grep "${1}" | grep -v grep | awk '{ n=split ($3,a,/\//); print a[n] }' ))
-		processCPUUsage=$(echo ${processCPUUsage[@]} | sed "s/ /+/g")
-		local totalCPUUsage=$(echo "scale=2;" ${processCPUUsage} | bc)
+		local processCPUUsage
+		local processCPUUsageArr
+		local psOutput
+		psOutput=$(ps aux)
+		processCPUUsageArr=($(echo "${psOutput}"  | grep "${1}" | grep -v grep | awk '{ n=split ($3,a,/\//); print a[n] }' ))
+		processCPUUsage=${processCPUUsageArr[*]}
+		processCPUUsage=${processCPUUsage// /+}
+		local totalCPUUsage
+		totalCPUUsage=$(echo "scale=2;" "${processCPUUsage}" | bc)
 		localSleepTime=$(echo "scale=2; (${totalCPUUsage}/100)" | bc)
-		ledTurnOn $2
-		sleep $localSleepTime
-		ledTurnOff $2
+		ledTurnOn "$2"
+		sleep "$localSleepTime"
+		ledTurnOff "$2"
 	done
 }
 
 monitorMemory() {
-	setTrigger $2 "none"
+	setTrigger "$2" "none"
 	while true; do
-		local processMemoryUsage=($(ps aux | grep "${1}" | grep -v grep | awk '{ n=split ($4,a,/\//); print a[n] }' ))
-		processMemoryUsage=$(echo ${processMemoryUsage[@]} | sed "s/ /+/g")
-		local totalMemoryUsage=$(echo "scale=2;" ${processMemoryUsage} | bc)
+		local processMemoryUsage
+		local processMemoryUsageArr
+		local psOutput
+		psOutput=$(ps aux)
+		processMemoryUsageArr=($(echo "${psOutput}" | grep "${1}" | grep -v grep | awk '{ n=split ($4,a,/\//); print a[n] }' ))
+		processMemoryUsage=${processMemoryUsageArr[*]}
+		processMemoryUsage=${processMemoryUsage// /+}
+		local totalMemoryUsage
+		totalMemoryUsage=$(echo "scale=2;" "${processMemoryUsage}" | bc)
 		localSleepTime=$(echo "scale=2; (${totalMemoryUsage}/100)" | bc)
-		ledTurnOn $2
-		sleep $localSleepTime
-		ledTurnOff $2
+		ledTurnOn "$2"
+		sleep "$localSleepTime"
+		ledTurnOff "$2"
 	done
 }
 
-main $@
+main "$@"
