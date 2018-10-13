8abba6b2cb989dd0f035d8bed77b159a7be7a490
Bradley Smith <s3318889@student.rmit.edu.au>
2018-10-13 11:19:40 +1100

    Comments

--- led_konfigurator.sh
+++ led_konfigurator.sh
@@ -4,12 +4,17 @@ LEDPATH=/sys/class/leds
 
 main() {
 	
+	# Display the LED Selection Menu
+	
 	while true; do
 		echo 'Welcome to Led_Konfigurator!'
 		echo '============================'
 		local ledFiles=()
 		local ledFilesCount=0
+		
+		# Get every file that represents an LED
 		for file in $LEDPATH/*; do
+			# Get the base name of the LED file which is the led name
 			local led=${file##*/}
 			ledFiles[ledFilesCount]=${led}
 			((ledFilesCount++))
@@ -17,6 +22,8 @@ main() {
 		done
 		local quitNo=$((ledFilesCount+1))
 		echo "${quitNo}. Quit"
+		
+		# Get user's selection
 		
 		local selection
 		read -r -p "Please enter a number (1-${quitNo}) for the led to configure or quit: " selection
@@ -30,6 +37,9 @@ main() {
 }
 
 ledManipulationMenu() {
+	
+	# Menu to manipulate user selected LED
+	
 	local led=$1
 	local selection
 	
@@ -43,6 +53,8 @@ ledManipulationMenu() {
 		echo '4. Associate with the performance of a process'
 		echo '5. Stop association with a process performance'
 		echo '6. Quit to main menu'
+		
+		# Get user selection
 		read -r -p 'Please enter a number (1-6) for your choice: ' selection
 	
 		case "$selection" in
@@ -69,16 +81,30 @@ ledManipulationMenu() {
 }
 
 ledTurnOff() {
+	# Make sure we have manual control of the LED
+	# by setting it's trigger to none
+	
 	setTrigger "$1" "none"
+	
+	# Value 0 turns the LED off
 	echo 0 > $LEDPATH/"$1"/brightness
 }
 
 ledTurnOn() {
+	# Make sure we have manual control of the LED
+	# by setting it's trigger to none
 	setTrigger "$1" "none"
+	
+	# Value 1 turns the LED on
 	echo 1 > $LEDPATH/"$1"/brightness
 }
 
 systemEventMenu() {
+	
+	# Menu to change the LED trigger
+	# This menu is paged so the menu text is built up in a string
+	# and then echoed to more
+	
 	local systemEventSelection
 	while true; do
 		local pageMenu=""
@@ -87,14 +113,23 @@ systemEventMenu() {
 		pageMenu="${pageMenu}"'Available events are:\n'
 		pageMenu="${pageMenu}"'---------------------\n'
 		local systemEvents=()
+		# Read in the triggers from the triggers file and
+		# add them into an array
 		read -r -a systemEvents <<< "$(getTriggers "$1")"
 		local counter=0
+		# Add each trigger to the menu with a number
+		# which is it's index in the array + 1
 		for event in "${systemEvents[@]}"; do
 			(( counter = counter + 1 ))
 			pageMenu="${pageMenu}${counter}) "
+			# Check if it is the current trigger for the LED
 			if [[ $event =~ ^\[.+\]$ ]]; then
+				# Remove the square brackets from it's name
 				event=$(echo "${event}" | sed -r -e 's/\[(.*)\]/\1/')
+				# Add it to the menu with an asterisk
 				pageMenu="${pageMenu}${event}*"'\n'
+				# Replace the name in the array with the plain name
+				# So it can be directly used later
 				systemEvents[$((counter - 1))]=${event}
 			else
 				pageMenu="${pageMenu}${event}"'\n'
@@ -102,7 +137,11 @@ systemEventMenu() {
 		done
 		quitNo=$((counter+1))
 		pageMenu="${pageMenu}${quitNo}) Quit to previous menu"
+		
+		# Page the menu with more
 		echo -e "${pageMenu}" | more
+		
+		# Get the user's selection
 		local systemEventSelection
 		read -r -p "Please select an option: (1-${quitNo}): " systemEventSelection
 		
@@ -118,14 +157,19 @@ systemEventMenu() {
 }
 
 getTriggers() {
+	# Read the trigger file to a string
 	cat "$LEDPATH/$1/trigger"
 }
 
 setTrigger() {
+	# Set the trigger by echoing it to the trigger file
 	echo "$2" > "$LEDPATH/$1/trigger"
 }
 
 associateProcessMenu() {
+	
+	# Menu to associate the LED with the performance of a process
+	
 	local processName
 	local monitorOption
 	local conflictChoice
@@ -136,8 +180,13 @@ associateProcessMenu() {
 	read -r -p "Please enter the name of the program to monitor(partial names are ok): " processName
 	read -r -p "Do you wish to 1) monitor memory or 2) monitor cpu? [enter memory or cpu]: " monitorOption
 	
+	# Read the list of processes, get only the process base name,
+	# search for the user's entered process name and show
+	# it only once if there is more than one of the process.
+	# All stored in an array.
 	matches=($(ps aux | awk '{ n=split ($11,a,/\//); print a[n] }' | grep "${processName}" | sort -u ))
 	processName=${matches[0]}
+	# If there is more than one match display the conflict menu
 	if ((${#matches[@]} > 1)); then
 		echo 'Name Conflict'
 		echo '-------------'
@@ -157,6 +206,8 @@ associateProcessMenu() {
 		processName=${matches[((conflictChoice-1))]}
 	fi
 	
+	# Start the process monitor in the background with no output
+	
 	(./process_monitor_led_konfigurator.sh -l "${1}" -m "${monitorOption}" -p "${processName}" 2>&1 &)
 	
 }
@@ -164,9 +215,14 @@ associateProcessMenu() {
 unassociateProcess() {
 	local processID
 	local psOutput
+	
+	# Get the process list, find the process monitor and get it's pid
 	psOutput=$(ps aux)
 	processID=$(echo "${psOutput}" | grep 'process_monitor_led_konfigurator.sh' | grep -v grep | awk '{ print $2 }')
+	
+	# Kill the process
 	kill "$processID"
 }
 
+# Display the first menu
 main
--- process_monitor_led_konfigurator.sh
+++ process_monitor_led_konfigurator.sh
@@ -10,6 +10,7 @@ main() {
 	local led
 	local usage
 	usage="Usage: ${0##*/} [-l led name] [-p process name] [-m montor value]"
+	# Use getopts to process the command line options
 	while getopts "l:p:m:" opt; do
 		case "$opt" in
 			l)
@@ -27,6 +28,8 @@ main() {
 				;;
 		esac
 	done
+	
+	# Check that correct arguments have been passed in
 	
 	if [ -z "$led" ]; then
 		echo 'error: must provide a led name'
@@ -62,31 +65,55 @@ main() {
 }
 
 setTrigger() {
+	# Set the trigger by echoing it to the trigger file
 	echo "$2" > "$LEDPATH/$1/trigger"
 }
 
 ledTurnOff() {
+	# Make sure we have manual control of the LED
+	# by setting it's trigger to none
+	
 	setTrigger "$1" "none"
+	
+	# Value 0 turns the LED off
 	echo 0 > $LEDPATH/"$1"/brightness
 }
 
 ledTurnOn() {
+	# Make sure we have manual control of the LED
+	# by setting it's trigger to none
 	setTrigger "$1" "none"
+	
+	# Value 1 turns the LED on
 	echo 1 > $LEDPATH/"$1"/brightness
 }
 
 monitorCPU() {
+	# Monitor the CPU usage of all the processes with the name
+	# given to the script
+	
 	setTrigger "$2" "none"
+	
 	while true; do
 		local processCPUUsage
 		local processCPUUsageArr
 		local psOutput
+		
+		# Get the process list, find all processes with the correct name
+		# get their cpu percentage and store them all in an array
 		psOutput=$(ps aux)
 		processCPUUsageArr=($(echo "${psOutput}"  | grep "${1}" | grep -v grep | awk '{ n=split ($3,a,/\//); print a[n] }' ))
+		
+		# Expand the array to a string and then replace the spaces
+		# generated between each value with a plus sign so it can
+		# be used with bc
 		processCPUUsage=${processCPUUsageArr[*]}
 		processCPUUsage=${processCPUUsage// /+}
+		
 		local totalCPUUsage
+		# Pass the string processed for bc into bc
 		totalCPUUsage=$(echo "scale=2;" "${processCPUUsage}" | bc)
+		# 100% = 1 second of sleep
 		localSleepTime=$(echo "scale=2; (${totalCPUUsage}/100)" | bc)
 		ledTurnOn "$2"
 		sleep "$localSleepTime"
@@ -100,12 +127,23 @@ monitorMemory() {
 		local processMemoryUsage
 		local processMemoryUsageArr
 		local psOutput
+		
+		# Get the process list, find all processes with the correct name
+		# get their memory percentage and store them all in an array
 		psOutput=$(ps aux)
 		processMemoryUsageArr=($(echo "${psOutput}" | grep "${1}" | grep -v grep | awk '{ n=split ($4,a,/\//); print a[n] }' ))
+		
+		# Expand the array to a string and then replace the spaces
+		# generated between each value with a plus sign so it can
+		# be used with bc
 		processMemoryUsage=${processMemoryUsageArr[*]}
 		processMemoryUsage=${processMemoryUsage// /+}
+		
+		
 		local totalMemoryUsage
+		# Pass the string processed for bc into bc
 		totalMemoryUsage=$(echo "scale=2;" "${processMemoryUsage}" | bc)
+		# 100% = 1 second of sleep
 		localSleepTime=$(echo "scale=2; (${totalMemoryUsage}/100)" | bc)
 		ledTurnOn "$2"
 		sleep "$localSleepTime"
